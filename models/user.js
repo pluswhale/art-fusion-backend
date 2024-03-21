f7543bceccba3326017b803215b48c6056812fd2
Egor Dultsev <listenmy@yandex.ru>
2024-03-21 14:18:20 +0300

    fix user model

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.hasMany(models.UserItem, { foreignKey: 'userId', as: 'items' });
+      User.hasMany(models.UserNFT, { foreignKey: 'userId', as: 'items' });
     }
   }
   User.init({
