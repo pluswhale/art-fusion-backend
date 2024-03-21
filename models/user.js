257abe40ae99407fe85d3692007411dc4b2aa2c7
Egor Dultsev <listenmy@yandex.ru>
2024-03-21 14:14:27 +0300

    chamnge user model

--- models/user.js
+++ models/user.js
@@ -15,14 +15,21 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   User.init({
-    id: DataTypes.INTEGER,
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true, // Explicitly mark the 'id' field as a primary key
+      autoIncrement: true, // Automatically increment the value
+      allowNull: false
+    },
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
+    // createdAt and updatedAt are automatically added by Sequelize for 'timestamps: true'
   }, {
     sequelize,
     modelName: 'User',
+    timestamps: true, // Enable Sequelize to auto manage createdAt and updatedAt
   });
   return User;
 };
